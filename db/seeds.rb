89ebe87f0a52af1e2d8176c5471e9de42ddde74e
Joel Sommers <jsommers@colgate.edu>
2022-09-26 16:45:20 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Book.destroy_all
+
+Book.create!(title: "ESaaS, first edition", pages: 234, publisher: "Strawberry Canyon", year: 2015, list_price: 19.99)
+Book.create!(title: "Moby Dick", pages: 999, publisher: "Unknown", year: 1830, list_price: 9.99)
+Book.create!(title: "The Brothers Karamazov", pages: 1200, publisher: "Unknown", year: 1875, list_price: 12.99)
+Book.create!(title: "Crime and Punishment", pages: 900, publisher: "Unknown", year: 1877, list_price: 10.99)
+Book.create!(title: "What we owe to each other", pages: 432, publisher: "Belknap Press", year: 2000, list_price: 31.39)
+Book.create!(title: "Let's go (so we can get back)", pages: 287, year: 2018, publisher: "Dutton", list_price: 19.19)
+Book.create!(title: "The essential Calvin and Hobbes", pages: 255, year: 1988, publisher: "Andrew and McMeel Books", list_price: 13.13)
