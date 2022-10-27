16ce7b16c8180d584db8d5e4b417d399c174ee28
Joel Sommers <jsommers@colgate.edu>
2022-10-27 11:09:54 -0400

    add reviews

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
+  has_many :reviews
+
   def self.title_search(string)
     Book.where("title like ?", "%#{string}%")
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_27_154949) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_27_142646) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.integer "pages"
@@ -21,4 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_27_154949) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "stars"
+    t.text "description"
+    t.integer "book_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
+  end
+
+  add_foreign_key "reviews", "books"
 end
